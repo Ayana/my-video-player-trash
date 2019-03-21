eb632f9aac759532727868d615012dbcf62e2397
Ayana Osawa <>
2019-03-20 21:22:47 -0400

    learned JSX

--- src/index.js
+++ src/index.js
@@ -1,2 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom'
+
+
+
+const App = () => {
+  const buttonText = { text: 'Click me'};
+  const style = {backgroundColor: 'blue',color:'#fff'};
+
+  return (
+    <div>
+      <label className="label" htmlFor="name">Enter name:</label>
+      <input id="name" type="text" />
+      <button style={style}>
+        {buttonText.text}
+      </button>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
